2fe963876e82dc2c8354135704fb607036a48bb3
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-09-02 12:36:14 +0200

    add DefaultLayout Header and Footer scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import Home from "./pages/Home";
 import TeasList from "./pages/TeasList";
 import TeaDetail from "./pages/TeaDetail";
 import About from "./pages/About";
+import DefaultLayout from "./layouts/DefaultLayout";
 
 export default function App() {
   return <BrowserRouter>
-    <Routes>
+    <Routes element={<Outlet />}>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/teas" element={<TeasList />} />
