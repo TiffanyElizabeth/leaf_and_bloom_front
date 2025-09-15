affb1dac3a2121f4ebf9226cdf31c7caa1fb5266
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-09-15 13:09:18 +0200

    added explanation Banner

--- src/components/ui/Banner.jsx
+++ src/components/ui/Banner.jsx
@@ -1,25 +1,23 @@
+// reusable componentso doefor page banners and heros 
+
 import Text from "./Text";
 
-export default function Banner({ title, subtitle, imageSrc, variant = "banner" }) {
-    // Set height based on variant
+export default function Banner({ title, subtitle, imageSrc, variant = "banner", className = "", children, ...props }) {
+    // Set height based on variant (default is banner at 300px)
     const height = variant === "hero" ? "600px" : "300px";
 
     return (
         <div
-            className="banner banner-image d-flex flex-column justify-content-center align-items-center text-center"
+            className={`banner banner-image d-flex flex-column justify-content-center align-items-center text-center ${className}`}
             style={{
                 height,
                 backgroundImage: `url(${imageSrc})`,
-
-
             }}
+            {...props}
         >
-            {title && <Text level={1} className="mb-2" style={{ color: "#fff" }}>{title}</Text>}
-
-            {/* Only show subtitle if  hero */}
-            {variant === "hero" && subtitle && (
-                <Text level={3} className="text-white">{subtitle}</Text>
-            )}
+            {title && <Text level={1} className="mb-2 text-white">{title}</Text>}
+            {subtitle && <Text level={3} className="text-white">{subtitle}</Text>}
+            {children} {/* in case of any other content - ad es. buttons */}
         </div>
     );
 }
