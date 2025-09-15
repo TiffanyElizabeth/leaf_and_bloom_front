ba63a6344eef9629afcedfa7e0856961420c01de
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-09-15 12:42:02 +0200

    added explanation Container

--- src/components/ui/Container.jsx
+++ src/components/ui/Container.jsx
@@ -1,5 +1,11 @@
-export default function Container({ children }) {
+// reusable layout component that reduces duplicate code - applies uniform container where it is imported 
+
+export default function Container({ children, className = "", ...props }) { //
     return (
-        <div className={`container mx-auto py-5`}>{children}</div>
+        <div className={`container mx-auto py-5 ${className}`} {...props}>{children}</div>
     )
 }
+
+// children = what goes in the container 
+// added className = "" to parameters so that can add additional classes in components (like bg-sage) and it won't overwrite my other classes - default is an empty string so that it works also if i don't add any other classes
+// ...props any other attributes a component passes will be applied 
