f13b7b3ff6610d672d84f5dc386a5196b891a15b
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-09-15 12:44:50 +0200

    added DividingLine explanation

--- src/components/ui/DividingLine.jsx
+++ src/components/ui/DividingLine.jsx
@@ -1,3 +1,4 @@
+// simple reusable dividing line that remains constant wherever it is used (have not allowed arguments to be passed because I want it to remain consistent throughout the app )
 export default function DividingLine() {
     return (
         <hr className="w-25 mx-auto mb-3" />
