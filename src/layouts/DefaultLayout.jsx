392a1d882d713a8992b19cf22b0eefc06218fa05
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-09-16 13:21:25 +0200

    added explanation DefaultLayout

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,4 +1,6 @@
-import { Outlet } from "react-router";
+// layout component that wraps pages with a consistent header and footer - uses Outlet as a placeholder for page-specific content. Allows us to avoid repeated imports of Header and Footer
+
+import { Outlet } from "react-router"; // a placeholder from React Router - all child routes of route using DefaultLayout will render where Outlet is
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
