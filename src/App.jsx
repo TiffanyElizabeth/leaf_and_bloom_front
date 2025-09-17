e0ed1f282166f8fc82acebf6c4cb605d2544fa39
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-09-17 10:52:49 +0200

    added explanation App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+// connects URL paths to the right page components (wrapping most of them in the same layout)
+
+//componente radice dell’applicazione. Contiene la struttura di base e organizza gli altri componenti per la visualizzazione dell’applicazione. Viene importato e renderizzato dal main.jsx, che e il punto d’ingresso dell’applicazione, rende l’applicazione come <App /> nel HTML DOM, specificamente nel elemento di radice definito dentro index.html
+
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import TeasList from "./pages/TeasList";
@@ -9,7 +13,7 @@ import DesignSystem from "./pages/DesignSystem";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter> {/* changes pages based on url (&permits us to change page without reloading the whole app */}
       <Routes>
         <Route path="/design-system" element={<DesignSystem />} />
         <Route element={<DefaultLayout />}>
